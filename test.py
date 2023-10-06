494062a5b01fd3bd1e96d4c6fd3cc8a6c4ab7f44
Akinyanmodi <akinyanseunwen@gmail.com>
2023-10-06 08:50:44 +0100

    Projects that helped me after taking python beginners courses

--- test.py
+++
@@ -1,6 +0,0 @@
-age = int(input("What's your year of birth? "))
-present_year = 2023
-present_age = f'{present_year - int(age)} years old'
-new_age = [present_year - int(age)] * str()
-print(present_age)
-print(new_age)
